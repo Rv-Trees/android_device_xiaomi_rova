f2f9d9b22335c8698907496572f4f45b2d91065d
Rve27 <rve27github@gmail.com>
2024-09-11 22:38:00 +0000

    rova: Drop 7SIM
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- 7SIM/.github/markdown/release-contributors.md
+++
@@ -1,4 +0,0 @@
-### :heart: Contributors
-
-Thank you to all the contributors who worked on this release:
-${CONTRIBUTORS}
--- 7SIM/.github/scripts/generate-settingslib-cache-key.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-set -euo pipefail
-
-url="$(./SettingsLib/fetch.sh --get-repo-url)"
-tag="$(./SettingsLib/fetch.sh --get-repo-tag)"
-key="$(echo -n "$url+$tag" | base64 --wrap=0)"
-echo "settingslibs-$key"
