57e18784f2b86ca239183ad82a539101fcaf05dd
Rve27 <rve27github@gmail.com>
2024-09-16 18:08:30 +0000

    rova: Change kernel branch
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone --recursive --depth=1 -b fourteen https://github.com/Rv-Trees/android_kernel_xiaomi_msm8937.git kernel/xiaomi/rova
+git clone --recursive --depth=1 -b mi8917 https://github.com/Rv-Trees/android_kernel_xiaomi_msm8937.git kernel/xiaomi/rova
 
 DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
